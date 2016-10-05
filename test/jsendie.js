0b7e435db821d7c9fc5137557752d0ca97ba1f07
Kelvin Tan <Kelvin@easywave.my>
2016-10-05 12:26:16 +0800

    Complete fail and error test script.

--- test/jsendie.js
+++ test/jsendie.js
@@ -21,4 +21,38 @@ describe("jsendie", () => {
             });
         });
     });
+
+    describe("fail", () => { 
+        it("should pass", function(done){
+            server(app)
+            .get('/fail')
+            .expect("Content-type", /json/)
+            .expect(200)
+            .end(function(err, res){
+                if(err) done(err);
+                res.status.should.equal(200);
+                res.body.status.should.equal("fail");
+                res.body.data.title.should.equal("A title is required");
+                
+                done();
+            });
+        });
+    });
+
+    describe("error", () => { 
+        it("should pass", function(done){
+            server(app)
+            .get('/error')
+            .expect("Content-type", /json/)
+            .expect(500)
+            .end(function(err, res){
+                if(err) done(err);
+                res.status.should.equal(500);
+                res.body.status.should.equal("error");
+                res.body.message.should.equal("Unable to communicate with database");
+                
+                done();
+            });
+        });
+    });
 });
